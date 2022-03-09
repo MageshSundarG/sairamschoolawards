98ed6f12ebdf5a93a07d64931efa03510e8e0734
magesh-sundar <mahesh@gfints.com>
2022-03-10 00:36:31 +0530

    bug fix : fixed airtable form alignment and adjustable height

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,18 @@ export default function Home() {
       <Head>
         <title>Sairam School Awards 2022</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
       </Head>
 
-      <main>
-        <iframe
-          class="airtable-embed"
-          src="https://airtable.com/embed/shreGsqHV7ztONvKr?backgroundColor=cyan"
-          frameborder="0"
-          onmousewheel=""
-          width="100%"
-          height="533"
-          style={{ background: 'transparent', border: '1px solid #ccc' }}
-        ></iframe>
-      </main>
+      <iframe
+        class="airtable-embed airtable-dynamic-height"
+        src="https://airtable.com/embed/shreGsqHV7ztONvKr?backgroundColor=cyan"
+        frameborder="0"
+        onmousewheel=""
+        width="100%"
+        height="743"
+        style={{ background: 'transparent', border: '1px solid #ccc' }}
+      ></iframe>
     </div>
   );
 }
