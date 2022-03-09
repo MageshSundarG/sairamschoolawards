1db8c05a16ca4195a2b8481fc84d5da28077601a
magesh-sundar <mahesh@gfints.com>
2022-03-10 00:00:59 +0530

    added the embeded airtable form

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Sairam School Awards 2022</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
       </Head>
 
       <main>
-        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shreGsqHV7ztONvKr?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="761" style="background: transparent; border: 1px solid #ccc;"></iframe>
+        <Header title="Welcome to Sairam School Awards 2022!" />
+        <iframe
+          class="airtable-embed airtable-dynamic-height"
+          src="https://airtable.com/embed/shreGsqHV7ztONvKr?backgroundColor=cyan"
+          frameborder="0"
+          onmousewheel=""
+          width="100%"
+          height="761"
+          style={{ background: 'transparent', border: '1px solid #ccc' }}
+        ></iframe>
       </main>
       <Footer />
     </div>
-  )
+  );
 }
